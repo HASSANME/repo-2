c22a66a832d49c802c85d733963688d476bdcd91
HASSANME <hassan.elouarjani@infomineo.com>
2025-12-24 13:53:10 +0100

    Modify Flask app to use dynamic port from environment
    Updated app.run to use PORT environment variable.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return 'Welcome to Python Flask World V1.0 from CI/CD pipeline'
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # Use the PORT environment variable provided by App Engine, default to 8080 for local testing
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
